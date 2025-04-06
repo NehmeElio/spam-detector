2082d32c2465ce93fd0443967c3258350650a465
Elio <elion628@gmail.com>
2025-04-06 08:23:54 +0300

    modified gitlab-ci v28

--- ml_service/app.py
+++ ml_service/app.py
@@ -8,13 +8,11 @@ import logging
 from prometheus_fastapi_instrumentator import Instrumentator
 import time
 
+# Set up logging
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-# Initialize FastAPI app
-app = FastAPI()
-
-# Add CORS middleware
+# Create FastAPI app instance
 app = FastAPI()
 
 # Add Prometheus middleware BEFORE app starts
@@ -30,9 +28,10 @@ app.add_middleware(
     allow_headers=["*"],  # Allow all headers
 )
 
-
+# Set up MLFlow tracking
 mlflow.set_tracking_uri("https://dagshub.com/NehmeElio/spam-detector.mlflow")
 
+# Model and vectorizer loading function
 def load_model_and_vectorizer():
     artifact_path_vectorizer = "vectorizer.pkl"
     uri_model = f"runs:/4e04fa7c9ad94d5e92f412ed2838fab8/model"
@@ -41,7 +40,7 @@ def load_model_and_vectorizer():
     artifact_local_path_vectorizer = mlflow.artifacts.download_artifacts(artifact_uri=artifact_uri_vectorizer, dst_path="ml_service/tmp/vectorizer")
     custom_path_vectorizer = f"ml_service/tmp/vectorizer/vectorizer.pkl"
 
-
+    # Log the loading process
     logger.info("Loading model and vectorizer...")
     model = mlflow.sklearn.load_model(model_uri=uri_model)
     with open(custom_path_vectorizer, 'rb') as f:
@@ -52,27 +51,30 @@ def load_model_and_vectorizer():
 
 model, vectorizer = load_model_and_vectorizer()
 
+# Define the input data model for FastAPI
 class TextData(BaseModel):
     text: str
 
-
-
+# Prediction endpoint with logging and MLFlow metric tracking
 @app.post("/predict")
 def predict_spam_or_ham(data: TextData) -> Dict[str, str]:
-    start_time = time.time()
+    start_time = time.time()  # Start timer for prediction
 
     # Log the incoming request
     logger.info(f"Received request: {data.text}")
 
+    # Transform the text and make a prediction
     text_transformed = vectorizer.transform([data.text])
     prediction = model.predict(text_transformed)
     result = 'spam' if prediction[0] == 1 else 'ham'
 
+    # Log prediction result
     logger.info(f"Prediction: {result}")
 
+    # Log metrics in MLFlow
     prediction_time = time.time() - start_time
-    mlflow.log_metric("prediction_time", prediction_time) 
-    mlflow.log_metric("prediction_count", 1) 
+    mlflow.log_metric("prediction_time", prediction_time)  # Log the prediction time
+    mlflow.log_metric("prediction_count", 1)  # Increment the prediction count
 
     # Return the prediction result
     return {"prediction": result}
